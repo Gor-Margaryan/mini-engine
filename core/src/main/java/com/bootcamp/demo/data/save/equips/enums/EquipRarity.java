6d06b38cf111a46da00e8f11b582d5279a4ff605
Gor-Margaryan <gor.margaryan700@gmail.com>
2025-04-30 16:26:45 +0400

    YUHUU

--- core/src/main/java/com/bootcamp/demo/data/save/equips/enums/EquipRarity.java
+++ core/src/main/java/com/bootcamp/demo/data/save/equips/enums/EquipRarity.java
@@ -4,17 +4,17 @@ import com.badlogic.gdx.graphics.Color;
 import lombok.Getter;
 
 public enum EquipRarity {
-    RUSTED(1, Color.valueOf("#d4a373")), RUSTED_2(2, Color.valueOf("#d4a373")),
-    SCRAP(1, Color.valueOf("#00b4d8")), SCRAP_2(2, Color.valueOf("#00b4d8")),
-    HARDENED(1, Color.valueOf("#ff99c8")), HARDENED_2(2, Color.valueOf("#ff99c8")),
-    ELITE(1, Color.valueOf("#ffbe0b")), ELITE_2(2, Color.valueOf("#ffbe0b")),
-    ASCENDANT(1, Color.valueOf("#ef233c")),ASCENDANT_2(2, Color.valueOf("#ef233c")),
-    NUCLEAR(1, Color.valueOf("#9ef01a")),
-    JUGGERNAUT(1, Color.valueOf("#3c096c")),
-    DOMINION(1, Color.valueOf("#ff3c38")),
-    OBLIVION(1, Color.valueOf("#7ae582")),
-    IMMORTAL(1, Color.valueOf("#59d2fe")),
-    ETHEREAL(1, Color.valueOf("#ffee70"));
+    RUSTED(1, Color.valueOf("#b29985")), RUSTED_2(2, Color.valueOf("#b29985")),
+    SCRAP(1, Color.valueOf("#5f98c3")), SCRAP_2(2, Color.valueOf("#5f98c3")),
+    HARDENED(1, Color.valueOf("#b792c8")), HARDENED_2(2, Color.valueOf("#b792c8")),
+    ELITE(1, Color.valueOf("#e5ad4d")), ELITE_2(2, Color.valueOf("#e5ad4d")),
+    ASCENDANT(1, Color.valueOf("#d95955")),ASCENDANT_2(2, Color.valueOf("#d95955")),
+    NUCLEAR(1, Color.valueOf("#a7d434")),
+    JUGGERNAUT(1, Color.valueOf("#796deb")),
+    DOMINION(1, Color.valueOf("#fb647d")),
+    OBLIVION(1, Color.valueOf("#4effba")),
+    IMMORTAL(1, Color.valueOf("#68ffdd")),
+    ETHEREAL(1, Color.valueOf("#fffcd1"));
 
 
     @Getter
