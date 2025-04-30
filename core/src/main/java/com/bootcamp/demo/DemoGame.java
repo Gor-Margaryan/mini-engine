6a58570c5aadd025c5ef27878bffc4689c2117b2
Gor-Margaryan <gor.margaryan700@gmail.com>
2025-04-30 17:01:45 +0400

    Dzzzzz

--- core/src/main/java/com/bootcamp/demo/DemoGame.java
+++ core/src/main/java/com/bootcamp/demo/DemoGame.java
@@ -4,20 +4,16 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.JsonWriter;
 import com.bootcamp.demo.data.game.GameData;
-import com.bootcamp.demo.data.game.equips.enums.EquipType;
 import com.bootcamp.demo.data.save.SaveData;
-import com.bootcamp.demo.data.save.equips.EquipSaveData;
-import com.bootcamp.demo.data.save.equips.enums.EquipRarity;
-import com.bootcamp.demo.data.save.stats.StatsSaveData;
-import com.bootcamp.demo.data.save.stats.enums.Stat;
 import com.bootcamp.demo.events.GameStartedEvent;
-import com.bootcamp.demo.managers.API;
 import com.bootcamp.demo.events.core.EventModule;
-
-
-import static com.badlogic.gdx.math.MathUtils.random;
+import com.bootcamp.demo.managers.API;
+import com.bootcamp.demo.pages.my.RandomEquipSaveData;
 
 public class DemoGame extends Game {
 
@@ -30,92 +26,14 @@ public class DemoGame extends Game {
         gameData.load();
 
         loadSaveData();
-        EquipRarity[] values = EquipRarity.values();
 
-        final EquipSaveData equipSaveData = new EquipSaveData();
-        equipSaveData.setName("weapon");
-        equipSaveData.setLevel(random.nextInt(1, 50));
-        equipSaveData.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData.setPower(random.nextFloat(1,5));
 
-        final StatsSaveData statsSaveData = new StatsSaveData();
-        ObjectMap<Stat, Float> stats = statsSaveData.getStats();
-        stats.put(Stat.HP, random.nextFloat(1,5));
-        stats.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData.setStats(stats);
-        equipSaveData.setStatsSaveData(statsSaveData);
-
-        final EquipSaveData equipSaveData1 = new EquipSaveData();
-        equipSaveData1.setName("melee");
-        equipSaveData1.setLevel(random.nextInt(1, 50));
-        equipSaveData1.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData1.setPower(random.nextFloat(1,5));
-
-        final StatsSaveData statsSaveData1 = new StatsSaveData();
-        ObjectMap<Stat, Float> stats1 = statsSaveData1.getStats();
-        stats1.put(Stat.HP, random.nextFloat(1,5));
-        stats1.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData1.setStats(stats1);
-        equipSaveData1.setStatsSaveData(statsSaveData1);
-
-        final EquipSaveData equipSaveData2 = new EquipSaveData();
-        equipSaveData2.setName("head");
-        equipSaveData2.setLevel(random.nextInt(1, 50));
-        equipSaveData2.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData2.setPower(random.nextFloat(1,5));
-
-        final StatsSaveData statsSaveData2 = new StatsSaveData();
-        ObjectMap<Stat, Float> stats2 = statsSaveData2.getStats();
-        stats2.put(Stat.HP, random.nextFloat(1,5));
-        stats2.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData2.setStats(stats2);
-        equipSaveData2.setStatsSaveData(statsSaveData2);
-
-        final EquipSaveData equipSaveData3 = new EquipSaveData();
-        equipSaveData3.setName("body");
-        equipSaveData3.setLevel(random.nextInt(1, 50));
-        equipSaveData3.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData3.setPower(random.nextFloat(1,5));
-
-        final StatsSaveData statsSaveData3 = new StatsSaveData();
-        ObjectMap<Stat, Float> stats3 = statsSaveData3.getStats();
-        stats3.put(Stat.HP, random.nextFloat(1,5));
-        stats3.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData3.setStats(stats3);
-        equipSaveData3.setStatsSaveData(statsSaveData3);
-
-        final EquipSaveData equipSaveData4 = new EquipSaveData();
-        equipSaveData4.setName("gloves");
-        equipSaveData4.setLevel(random.nextInt(1, 50));
-        equipSaveData4.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData4.setPower(random.nextFloat(1,5));
-
-        final StatsSaveData statsSaveData4 = new StatsSaveData();
-        ObjectMap<Stat, Float> stats4 = statsSaveData4.getStats();
-        stats4.put(Stat.HP, random.nextFloat(1,5));
-        stats4.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData4.setStats(stats4);
-        equipSaveData4.setStatsSaveData(statsSaveData4);
-
-        final EquipSaveData equipSaveData5 = new EquipSaveData();
-        equipSaveData5.setName("shoes");
-        equipSaveData5.setLevel(random.nextInt(1, 50));
-        equipSaveData5.setEquipRarity(values[random.nextInt(0,15)]);
-        equipSaveData5.setPower(random.nextFloat(1,5));
-
-        final StatsSaveData statsSaveData5 = new StatsSaveData();
-        ObjectMap<Stat, Float> stats5 = statsSaveData.getStats();
-        stats5.put(Stat.HP, random.nextFloat(1,5));
-        stats5.put(Stat.ATK, random.nextFloat(1,5));
-        statsSaveData5.setStats(stats5);
-        equipSaveData5.setStatsSaveData(statsSaveData5);
-
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.WEAPON, equipSaveData);
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.MELEE, equipSaveData1);
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.HEAD, equipSaveData2);
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.BODY, equipSaveData3);
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.GLOVES, equipSaveData4);
-        API.get(SaveData.class).getEquipsSaveData().getEquips().put(EquipType.SHOES, equipSaveData5);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
+        RandomEquipSaveData.getRandomEquipSaveData(gameData);
 
         savePlayerData();
 
---
+++ core/src/main/java/com/bootcamp/demo/pages/my/RandomEquipSaveData.java
@@ -0,0 +1,39 @@
+package com.bootcamp.demo.pages.my;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.bootcamp.demo.data.game.GameData;
+import com.bootcamp.demo.data.game.equips.EquipGameData;
+import com.bootcamp.demo.data.save.SaveData;
+import com.bootcamp.demo.data.save.equips.EquipSaveData;
+import com.bootcamp.demo.data.save.equips.enums.EquipRarity;
+import com.bootcamp.demo.data.save.stats.StatsSaveData;
+import com.bootcamp.demo.data.save.stats.enums.Stat;
+import com.bootcamp.demo.managers.API;
+
+import static com.badlogic.gdx.math.MathUtils.random;
+
+public class RandomEquipSaveData {
+    public static void getRandomEquipSaveData(GameData gameData) {
+        ObjectMap<String, EquipGameData> equips = gameData.getEquipsGameData().getEquips();
+        Array<String> array = equips.keys().toArray();
+        EquipRarity[] values = EquipRarity.values();
+        Stat[] stats = Stat.values();
+
+
+        EquipSaveData equipSaveData = new EquipSaveData();
+        equipSaveData.setName(array.random());
+        equipSaveData.setLevel(random.nextInt(1, 10));
+        equipSaveData.setEquipRarity(values[random.nextInt(0,15)]);
+        equipSaveData.setPower(random.nextFloat(1,5));
+
+        final StatsSaveData statsSaveData = new StatsSaveData();
+        ObjectMap<Stat, Float> stats1 = statsSaveData.getStats();
+        stats1.put(stats[random.nextInt(0,8)], random.nextFloat(1,5));
+        stats1.put(stats[random.nextInt(0,8)], random.nextFloat(1,5));
+        statsSaveData.setStats(stats1);
+        equipSaveData.setStatsSaveData(statsSaveData);
+
+        API.get(SaveData.class).getEquipsSaveData().getEquips().put(equips.get(equipSaveData.getName()).getEquipType(), equipSaveData);
+    }
+}
